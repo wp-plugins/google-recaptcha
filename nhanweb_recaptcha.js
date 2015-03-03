b7cf6e9a0d1ebc8e7b5585f8230c429f75fc905c
babyinternet <babyinternet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-03 01:02:09 +0000

    Upload plugin
    git-svn-id: https://plugins.svn.wordpress.org/google-recaptcha/trunk@1103690 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nhanweb_recaptcha.js
@@ -0,0 +1,16 @@
+function recaptcha_callback()
+{
+    var buttons = [
+        '#loginform #wp-submit',
+        '#lostpasswordform #wp-submit',
+        '#registerform #wp-submit',
+        '#commentform #submit'
+    ];
+    for(var i=0; i<buttons.length; i++)
+    {
+        if(jQuery(buttons[i]).length > 0)
+        {
+            jQuery(buttons[i]).show();
+        }
+    }
+}
